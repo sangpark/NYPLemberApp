6ea2e2b120bd7a882f3202749d3244966add50a7
NYCSwan <megan.swanby@gmail.com>
2017-01-15 08:10:40 -0500

    Add routes and templates for books, book, author objects

--- app/router.js
+++ app/router.js
@@ -7,6 +7,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('books', {}); //list of books
+  this.route('book', {path: 'book/:id'}, function() { //book index
+    this.route('author', {}, function() {}); //author index
+  });
+  this.route('notfound', {path: '*path'});
 });
 
 export default Router;
