8de14753ad3b634c0a57cad4075ab473933cba56
NYCSwan <megan.swanby@gmail.com>
2017-01-15 19:18:30 -0500

    Add book content to templates

--- app/routes/book.js
+++ app/routes/book.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return $.get(`http://api.repo.nypl.org/api/v1/items/search?q=${params.id}&publicDomainOnly=true`);
+debugger;
+  }
 });
--- app/routes/books.js
+++ app/routes/books.js
@@ -3,12 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return [
-     {id: "emberjs"},
-     {id: "ember-cli"},
-     {id: "microsoft"},
-     {id: "yahoo"},
-     {id: "netflix"},
-     {id: "facebook"}
+     {id: "cat"},
+     {id: "dog"},
+     {id: "horse"},
+     {id: "fish"},
+     {id: "people"},
+     {id: "bird"}
     ]
   }
 });
